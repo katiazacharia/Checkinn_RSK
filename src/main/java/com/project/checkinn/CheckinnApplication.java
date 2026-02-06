64fdf938566872ad3b09ff7f8c6f7fa29f528019
katiazacharia <katiazacharia87@gmail.com>
2026-02-06 15:48:32 +0200

    Add Spring Boot initial project structure

---
+++ src/main/java/com/project/checkinn/CheckinnApplication.java
@@ -0,0 +1,15 @@
+package com.project.checkinn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class CheckinnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckinnApplication.class, args);
+	}
+
+}
