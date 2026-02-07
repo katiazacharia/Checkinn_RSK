85e7eafe92f362512d716cd4954069c7b49d2fe1
katiazacharia <katiazacharia87@gmail.com>
2026-02-07 20:56:40 +0200

    booking reservation request dto class

--- src/main/java/com/project/checkinn/booking/reservation/BookingRequest.java
+++ src/main/java/com/project/checkinn/booking/reservation/BookingRequest.java
@@ -1,4 +1,28 @@
 package com.project.checkinn.booking.reservation;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
 public class BookingRequest {
+
+    private Long userId;
+    private LocalDate checkInDate;
+    private LocalDate checkOutDate;
+    private BigDecimal totalPrice;
+    private Long promoCodeId; // optional
+
+    public Long getUserId() { return userId; }
+    public void setUserId(Long userId) { this.userId = userId; }
+
+    public LocalDate getCheckInDate() { return checkInDate; }
+    public void setCheckInDate(LocalDate checkInDate) { this.checkInDate = checkInDate; }
+
+    public LocalDate getCheckOutDate() { return checkOutDate; }
+    public void setCheckOutDate(LocalDate checkOutDate) { this.checkOutDate = checkOutDate; }
+
+    public BigDecimal getTotalPrice() { return totalPrice; }
+    public void setTotalPrice(BigDecimal totalPrice) { this.totalPrice = totalPrice; }
+
+    public Long getPromoCodeId() { return promoCodeId; }
+    public void setPromoCodeId(Long promoCodeId) { this.promoCodeId = promoCodeId; }
 }
