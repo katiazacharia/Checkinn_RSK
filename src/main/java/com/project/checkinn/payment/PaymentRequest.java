ea5fcdbaebb8f774a1c021d5f12cd98acc54be92
RawandM-odeh <202302923@bethlehem.edu>
2026-02-07 17:48:46 +0200

    paymentrequest is ready , plz check it

--- src/main/java/com/project/checkinn/payment/PaymentRequest.java
+++ src/main/java/com/project/checkinn/payment/PaymentRequest.java
@@ -1,4 +1,24 @@
 package com.project.checkinn.payment;
 
 public class PaymentRequest {
+
+
+    private String reference;
+    private Double amount;
+    private String method;
+    private String status;
+
+    public PaymentRequest() {}
+
+    public String getReference() { return reference; }
+    public void setReference(String reference) { this.reference = reference; }
+
+    public Double getAmount() { return amount; }
+    public void setAmount(Double amount) { this.amount = amount; }
+
+    public String getMethod() { return method; }
+    public void setMethod(String method) { this.method = method; }
+
+    public String getStatus() { return status; }
+    public void setStatus(String status) { this.status = status; }
 }
