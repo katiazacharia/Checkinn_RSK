3ec3d2b050a2430d411c4196f8437c6efd38e1dd
ShahdHadya <hadyashahd5@gmail.com>
2026-02-08 23:58:48 +0200

    adding feilds for UserResponse class with their getters

--- src/main/java/com/project/checkinn/user/profile/UserResponse.java
+++ src/main/java/com/project/checkinn/user/profile/UserResponse.java
@@ -1,4 +1,32 @@
 package com.project.checkinn.user.profile;
 
 public class UserResponse {
+    private Long id;
+    private String fullName;
+    private String email;
+    private String phone;
+
+
+    public UserResponse(Long id, String fullName, String email, String phone) {
+        this.id = id;
+        this.fullName = fullName;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
 }
