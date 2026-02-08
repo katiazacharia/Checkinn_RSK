885f1e56afde097d65b0de576619effeecaf45de
RawandM-odeh <202302923@bethlehem.edu>
2026-02-08 20:14:56 +0200

    new classes are added, check it

--- src/main/java/com/project/checkinn/catalog/hotel/HotelCreateRequest.java
+++ src/main/java/com/project/checkinn/catalog/hotel/HotelCreateRequest.java
@@ -1,4 +1,30 @@
 package com.project.checkinn.catalog.hotel;
 
+import java.util.Set;
+
 public class HotelCreateRequest {
+    private String name;
+    private String city;
+    private String address;
+    private String description;
+
+
+    private Set<Long> amenityIds;
+
+    public HotelCreateRequest() {}
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getCity() { return city; }
+    public void setCity(String city) { this.city = city; }
+
+    public String getAddress() { return address; }
+    public void setAddress(String address) { this.address = address; }
+
+    public String getDescription() { return description; }
+    public void setDescription(String description) { this.description = description; }
+
+    public Set<Long> getAmenityIds() { return amenityIds; }
+    public void setAmenityIds(Set<Long> amenityIds) { this.amenityIds = amenityIds; }
 }
