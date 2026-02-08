b6042e568adea4783ca540e7837672c0a4151ccc
RawandM-odeh <202302923@bethlehem.edu>
2026-02-08 20:31:45 +0200

    starting to implement classes of pckg room

--- src/main/java/com/project/checkinn/catalog/room/RoomRepo.java
+++ src/main/java/com/project/checkinn/catalog/room/RoomRepo.java
@@ -1,4 +1,10 @@
 package com.project.checkinn.catalog.room;
 
-public class RoomRepo {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface RoomRepo extends JpaRepository<Room, Long> {
+    List<Room> findByHotelId(Long hotelId);
+    boolean existsByHotelIdAndRoomNumber(Long hotelId, String roomNumber);
 }
