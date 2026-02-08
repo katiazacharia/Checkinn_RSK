5673931fdaf482ccd4e4f61ee82a5bd17e41cff1
ShahdHadya <hadyashahd5@gmail.com>
2026-02-08 23:52:01 +0200

    prepared UserCreateRequest class

--- src/main/java/com/project/checkinn/user/profile/UserCreateRequest.java
+++ src/main/java/com/project/checkinn/user/profile/UserCreateRequest.java
@@ -1,4 +1,26 @@
 package com.project.checkinn.user.profile;
 
 public class UserCreateRequest {
+    private String fullName;
+    private String email;
+    private String phone;
+
+    public String getFullName() {
+        return fullName;
+    }
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getPhone() {
+        return phone;
+    }
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
 }
