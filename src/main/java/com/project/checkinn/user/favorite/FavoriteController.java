fa2dcb12da035e3b5abf7a97b4788812cd012613
ShahdHadya <hadyashahd5@gmail.com>
2026-02-11 00:27:42 +0200

    forgot the favorite controller

--- src/main/java/com/project/checkinn/user/favorite/FavoriteController.java
+++ src/main/java/com/project/checkinn/user/favorite/FavoriteController.java
@@ -1,4 +1,28 @@
 package com.project.checkinn.user.favorite;
 
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/favorites")
 public class FavoriteController {
+
+    private final FavoriteService favoriteService;
+
+    public FavoriteController(FavoriteService favoriteService) {
+        this.favoriteService = favoriteService;
+    }
+    @PostMapping
+    public FavoriteResponse add(
+            @RequestParam Long userId,
+            @RequestParam Long itemId
+    ) {
+        return favoriteService.add(userId, itemId);
+    }
+    @GetMapping("/user/{userId}")
+    public List<FavoriteResponse> getByUser(@PathVariable Long userId) {
+        return favoriteService.getByUser(userId);
+    }
 }
