df0b780c91ca75b7510da3de1fdeccb6b7b762a8
RawandM-odeh <202302923@bethlehem.edu>
2026-02-10 22:21:46 +0200

    controller class is implemented and i edit class payment request

--- src/main/java/com/project/checkinn/payment/PaymentController.java
+++ src/main/java/com/project/checkinn/payment/PaymentController.java
@@ -1,4 +1,49 @@
 package com.project.checkinn.payment;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/payments")
 public class PaymentController {
-}
+
+
+
+        private final PaymentService paymentService;
+
+        public PaymentController(PaymentService paymentService) {
+            this.paymentService = paymentService;
+        }
+
+
+        @PostMapping
+        @ResponseStatus(HttpStatus.CREATED)
+        public PaymentResponse create(@RequestBody PaymentRequest request) {
+
+            Payment payment = paymentService.create(
+                    request.getBookingId(),
+                    request.getAmount(),
+                    request.getMethod()
+            );
+
+            return PaymentMapper.toResponse(payment);
+        }
+
+        @GetMapping("/{id}")
+        public PaymentResponse getById(@PathVariable Long id) {
+            return PaymentMapper.toResponse(paymentService.getById(id));
+        }
+
+
+        @GetMapping
+        public List<PaymentResponse> getAll() {
+            return paymentService.getAll()
+                    .stream()
+                    .map(PaymentMapper::toResponse)
+                    .toList();
+        }
+
+
+    }
