18ef5617cca5a6e04ee146a1fdc535d2a5c0341d
ShahdHadya <hadyashahd5@gmail.com>
2026-02-19 15:59:01 +0200

    added some methods in the notification service

--- src/main/java/com/project/checkinn/notification/NotificationRepository.java
+++ src/main/java/com/project/checkinn/notification/NotificationRepository.java
@@ -1,6 +1,17 @@
 package com.project.checkinn.notification;
 
+import com.project.checkinn.common.NotificationStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
+
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
+
+    List<Notification> findByUser_IdOrderBySentAtDesc(Long userId);
+
+    List<Notification> findByUser_IdAndStatusOrderBySentAtDesc(Long userId, NotificationStatus status);
+
+    long countByUser_IdAndStatus(Long userId, NotificationStatus status);
+
 }
--- src/main/java/com/project/checkinn/notification/NotificationService.java
+++ src/main/java/com/project/checkinn/notification/NotificationService.java
@@ -1,5 +1,6 @@
 package com.project.checkinn.notification;
 
+import com.project.checkinn.common.NotificationStatus;
 import com.project.checkinn.common.NotificationType;
 import java.util.List;
 
@@ -13,7 +14,21 @@ public interface NotificationService {
             String message
     );
 
+    Notification createFromRequest(NotificationRequest request);
+
     Notification getById(Long id);
 
     List<Notification> getAll();
+
+    List<Notification> getByUser(Long userId);
+
+    List<Notification> getByUserAndStatus(Long userId, NotificationStatus status);
+
+    Notification updateStatus(Long id, NotificationStatus status);
+
+    Notification markRead(Long id);
+
+    void markReadAll(Long userId);
+
+    void delete(Long id);
 }
