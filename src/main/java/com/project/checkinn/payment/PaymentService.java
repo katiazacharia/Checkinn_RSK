e2b0d1c9ed5ef2e2e7eb1f51236dce30be633b5e
RawandM-odeh <202302923@bethlehem.edu>
2026-02-23 18:18:56 +0200

    i added new methods and endpoints

--- src/main/java/com/project/checkinn/payment/PaymentService.java
+++ src/main/java/com/project/checkinn/payment/PaymentService.java
@@ -1,6 +1,8 @@
 package com.project.checkinn.payment;
 
 import com.project.checkinn.common.PaymentMethod;
+import com.project.checkinn.common.PaymentStatus;
+
 import java.math.BigDecimal;
 import java.util.List;
 
@@ -15,4 +17,10 @@ public interface PaymentService {
     Payment getById(Long id);
 
     List<Payment> getAll();
+
+    Payment getByBookingId(Long bookingId);
+
+    List<Payment> search(Long bookingId, PaymentStatus status, PaymentMethod method);
+
+    Payment updateStatus(Long id, PaymentStatus status);
 }
