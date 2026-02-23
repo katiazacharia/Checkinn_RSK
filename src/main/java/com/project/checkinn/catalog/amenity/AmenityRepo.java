84723569042e95502a20a368864fd9b071958c6a
RawandM-odeh <202302923@bethlehem.edu>
2026-02-23 19:55:32 +0200

    amenities checkup

--- src/main/java/com/project/checkinn/catalog/amenity/AmenityRepo.java
+++ src/main/java/com/project/checkinn/catalog/amenity/AmenityRepo.java
@@ -1,10 +1,14 @@
 package com.project.checkinn.catalog.amenity;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface AmenityRepo extends JpaRepository<Amenity, Long> {
     boolean existsByNameIgnoreCase(String name);
-    Optional<Amenity> findByNameIgnoreCase(String name);
+    Page<Amenity> findByNameContainingIgnoreCase(String name, Pageable pageable);
+    Page<Amenity> findByHotels_Id(Long hotelId, Pageable pageable);
+
 }
