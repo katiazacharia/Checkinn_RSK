27aee0434e46fee948eaf0ee42b87b384253b60e
ShahdHadya <hadyashahd5@gmail.com>
2026-03-12 14:05:48 +0200

    updated classes and added the refresh token class with its implementation

---
+++ src/main/java/com/project/checkinn/security/RefreshTokenRepository.java
@@ -0,0 +1,17 @@
+package com.project.checkinn.security;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+    Optional<RefreshToken> findByToken(String token);
+
+    void deleteByUser(AppUser user);
+
+    void deleteByToken(String token);
+
+}
